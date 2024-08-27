88badd82e084856269b4ab3876e64b376ae93d00
zhanglan <874259005@qq.com>
2024-08-27 21:10:57 +0800

    ✨ 增加备案信息

---
+++ blogFrontend/src/components/beian/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="beian-content" @click="onClickBeian">
+        蜀ICP备2023030024号-1
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        const onClickBeian = () => {
+            window.open('https://beian.miit.gov.cn/')
+        }
+
+        return {
+            onClickBeian
+        }
+    }
+})
+</script>
+
+<style scoped>
+    .beian-content {
+        position: fixed;
+        bottom: 1vh;
+        left: 50%;
+        transform: translateX(-50%);
+        color: #fff;
+        cursor: pointer;
+        font-size: 12px;
+        opacity: 0.4;
+    }
+</style>
