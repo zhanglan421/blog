0168ee7a7ce4ed5be497ab5d666405deec0e2bf5
zhanglan <874259005@qq.com>
2024-08-28 15:31:30 +0800

    ✨ 首页重构

--- blogFrontend/src/App.vue
+++ blogFrontend/src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <vue-particles id="tsparticles" :options="tsparticlesConfig" />
-
-  <starBackground />
-
   <RouterView />
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-
-import starBackground from '@/components/starBackground/index.vue'
-
-import {tsparticlesConfig} from '@/config/tsparticles.config'
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+#tsparticles {
+  position: fixed;
+  z-index: 1;
+}
+</style>
--- blogFrontend/src/views/home/index.vue
+++ blogFrontend/src/views/home/index.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        你好
+        <div class="home-content">你好，我有一个帽衫，我要在网上问问</div>
+
+        <vue-particles id="tsparticles" :options="tsparticlesConfig" />
+
+        <starBackground />
+
         <beian />
     </div>
 </template>
@@ -9,19 +14,39 @@
 import { defineComponent } from 'vue'
 
 import beian from '@/components/beian/index.vue'
+import starBackground from '@/components/starBackground/index.vue'
+
+import { tsparticlesConfig } from '@/config/tsparticles.config'
 
 export default defineComponent({
     components: {
-        beian
+        beian,
+        starBackground
     },
-    setup () {
-        
+    setup() {
 
-        return {}
+
+        return {
+            tsparticlesConfig
+        }
     }
 })
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.home-content {
+    position: relative;
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-size: 30px;
+    z-index: 999;
+    
+    &::selection {
+        background-color: transparent;
+    }
+}
 </style>
